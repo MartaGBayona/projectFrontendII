d94924c809fb64e3c7afc388ae079f91bf34f540
MartaGBayona <marta.g.bayona@outlook.es>
2024-04-06 13:08:09 +0200

    feat: init to create Home and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { Header } from "./common/Header/Header"
+import { Body } from './pages/Body/Body'
 
 function App() {
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css";
+
+export const Home = () => {
+    return (
+        <div className="homeDesign">
+            Soy la página Home
+        </div>
+    )
+}
