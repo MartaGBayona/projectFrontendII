7e5833e427e9a9f03de258802db86cb0d6e6b621
MartaGBayona <marta.g.bayona@outlook.es>
2024-04-06 20:08:58 +0200

    update: modified functionalitie header and add searchSlice

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,21 +1,61 @@
 import { CustomLink } from "../CustomLink/CustomLink"
+import { CustomInput } from "../CustomInput/CustomInput";
+import { useState, useEffect } from "react";
 import "./Header.css"
 
+import { useSelector, useDispatch } from "react-redux";
+import { userData, logout } from "../../app/slices/userSlice";
+import { updateCriteria } from "../../app/slices/searchSlice";
+
 export const Header = () => {
+
+    const rdxUser = useSelector(userData);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        console.log(rdxUser, " credentials passport");
+    }, [rdxUser]);
+
+    const [criteria, setCriteria] = useState("");
+
+    const searchHandler = (e) => {
+        setCriteria(e.target.value);
+    };
+
+    useEffect(() => {
+        const searching = setTimeout(() => {
+            dispatch(updateCriteria(criteria));
+        }, 375);
+
+        return () => clearTimeout(searching);
+    }, [criteria]);
+
     return (
         <div className="headerDesign">
-            <CustomLink
-                path="/"
-                title="Home"
+            <CustomInput
+                type="text"
+                name="criteria"
+                value={criteria || ""}
+                changeEmit={searchHandler}
             />
-            <CustomLink
-                path="/login"
-                title="Login"
-            />
-            <CustomLink
-                path="/register"
-                title="Register"
-            />
+            <CustomLink path="/" title="Home" />
+            {rdxUser?.credentials?.token ? (
+                <div className="navigatorDesign">
+                    <CustomLink path="/profile" title={rdxUser?.credentials?.user?.name} />
+                    <div
+                        className="outDesign"
+                        onClick={() => dispatch(logout({ credentials: "" }))}
+                    >
+                        log out
+                    </div>
+                </div>
+            ) : (
+                <div className="navigatorDesign">
+                    <CustomLink path="/login" title="Login" />
+                    <CustomLink path="/register" title="Register" />
+                </div>
+            )}
         </div>
     )
 }
